611812744521c245cbe9ee147e3e5dcd76fdb2c8
Pham Van Tung <Phamtungkp2002@gmail.com>
2023-11-25 16:12:20 +0700

    first commit

---
+++ HomeWork_02/src/Session_02_Hw/B1_Hw.java
@@ -0,0 +1,24 @@
+package Session_02_Hw;
+
+import java.util.Scanner;
+
+public class B1_Hw {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first, last;
+        int total = 0;
+        System.out.println("Nhap vao gia tri dau tien:");
+        first = sc.nextInt();
+        System.out.println("Nhap vao gia tri cuoi cung:");
+        last = sc.nextInt();
+
+        //tinh tong cac so chan vua nhap
+        for (int i = first; i <= last; i++) {
+            if (i % 2 == 0) {
+                total += i;
+            }
+        }
+        System.out.println("Tong cua cac so chan tu "+first+
+                " den "+last+" la "+total);
+    }
+}
